fd2a733cafbde9603cd0701c20befd87ed6096f0
Brandan Schmitz <brandan.schmitz@celestialdata.net>
2025-02-26 21:32:32 -0600

    Remove stuff that should be in the other branch

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,4 +1,4 @@
-package frc.robot.subsystems.Elevator;
+package frc.robot.subsystems;
 
 import static edu.wpi.first.units.Units.Amp;
 import static edu.wpi.first.units.Units.Amps;
--- src/main/java/frc/robot/subsystems/Elevator/ElevatorIO.java
+++
@@ -1,6 +0,0 @@
-package frc.robot.subsystems.Elevator;
-
-import frc.robot.subsystems.GenericMotionProfiledSubsystem.GenericMotionProfiledSubsystemIO;
-
-public interface ElevatorIO extends GenericMotionProfiledSubsystemIO {
-}
